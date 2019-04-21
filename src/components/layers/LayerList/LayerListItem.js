8e0a094102fd0632d58af088086d3a09d319c0a3
Erick Otenyo <otenyo.erick@gmail.com>
2019-04-21 16:42:31 +0300

    refactor

--- src/components/layers/LayerList/LayerListItem.js
+++ src/components/layers/LayerList/LayerListItem.js
@@ -3,7 +3,7 @@ import { findDOMNode } from "react-dom";
 import { connect } from "react-redux";
 import { isLayerVisible, getLayerTitle } from "@boundlessgeo/sdk/util";
 import { updateLayer } from "@boundlessgeo/sdk/actions/map";
-import { fitExtent } from "@boundlessgeo/sdk/actions/map";
+import { fitExtent, removeLayer } from "@boundlessgeo/sdk/actions/map";
 import SdkLayerListItem from "@boundlessgeo/sdk/components/layer-list-item";
 import { DragSource, DropTarget } from "react-dnd";
 import {
@@ -25,57 +25,7 @@ import Tooltip from "@material-ui/core/Tooltip";
 import SortableHandle from "./SortableHandle";
 import LayerToolbar from "./toolbar/LayerToolbar";
 import { toggleDataTable } from "../../../actions/dataTable";
-
-const styles = {
-	card: {
-		position: "relative",
-		margin: "8px 4px 0 4px",
-		paddingBottom: 0,
-		zIndex: 2000
-	},
-	header: {
-		height: 54,
-		padding: "2px 8px 0 18px",
-		fontSize: 14
-	},
-	title: {
-		width: 227,
-		paddingLeft: 15,
-		fontSize: 15,
-		whiteSpace: "nowrap",
-		overflow: "hidden",
-		textOverflow: "ellipsis",
-		fontWeight: 500,
-		lineHeight: "17px"
-	},
-	subheader: {
-		width: 195,
-		paddingLeft: 15,
-		lineHeight: "17px",
-		fontSize: 14
-	},
-	legend: {
-		paddingLeft: 32
-	},
-	actions: {
-		backgroundColor: "#eee",
-		height: 32
-	},
-	visibility: {
-		position: "absolute",
-		right: 28,
-		top: 4
-	},
-	expand: {
-		position: "absolute",
-		right: -4,
-		top: 4
-	},
-	content: {
-		fontSize: 14,
-		padding: 0 // TODO: Not working on :last-child
-	}
-};
+import styles from "./styles";
 
 const collect = (connect, monitor) => {
 	return {
@@ -93,6 +43,15 @@ class LayerListItem extends SdkLayerListItem {
 			isMouseOver: false
 		};
 	}
+
+	removeLayer = () => {
+		const {
+			layer: { id },
+			dispatch
+		} = this.props;
+
+		dispatch(removeLayer(id));
+	};
 
 	toggleLayerVisibility = () => {
 		super.toggleVisibility();
@@ -182,6 +141,7 @@ class LayerListItem extends SdkLayerListItem {
 								opacity={opacity}
 								isVisible={isVisible}
 								fitToLayer={canFitToLayer && this.handleFitToLayerExtent}
+								removeLayer={this.removeLayer}
 								// onEdit={() => ({})}
 								toggleDataTable={() => toggleDataTable(layer.id)}
 								toggleLayerVisibility={this.toggleLayerVisibility}
---
+++ src/components/layers/LayerList/styles.js
@@ -0,0 +1,50 @@
+export default {
+	card: {
+		position: "relative",
+		margin: "8px 4px 0 4px",
+		paddingBottom: 0,
+		zIndex: 2000
+	},
+	header: {
+		height: 54,
+		padding: "2px 8px 0 18px",
+		fontSize: 14
+	},
+	title: {
+		width: 227,
+		paddingLeft: 15,
+		fontSize: 15,
+		whiteSpace: "nowrap",
+		overflow: "hidden",
+		textOverflow: "ellipsis",
+		fontWeight: 500,
+		lineHeight: "17px"
+	},
+	subheader: {
+		width: 195,
+		paddingLeft: 15,
+		lineHeight: "17px",
+		fontSize: 14
+	},
+	legend: {
+		paddingLeft: 32
+	},
+	actions: {
+		backgroundColor: "#eee",
+		height: 32
+	},
+	visibility: {
+		position: "absolute",
+		right: 28,
+		top: 4
+	},
+	expand: {
+		position: "absolute",
+		right: -4,
+		top: 4
+	},
+	content: {
+		fontSize: 14,
+		padding: 0 // TODO: Not working on :last-child
+	}
+};
